3b42670e08df4d3cc50a8d7590b65985c28bb194
unknown <giancarloestrella09@gmail.com>
2021-11-14 17:02:03 +0800

    Determines the lowest number among the set of given numbers using if-else statements.

---
+++ 2-lowestnumber.py
@@ -0,0 +1,13 @@
+# Find the lowest number
+# Create a program that ask 3 numbers. 
+# Find the lowest number using only if-else statement.
+# Display the lowest number
+
+# steps
+# 1. ask for three numbers
+first = int(input("Enter the first number here: "))
+second = int(input("Enter the second number here: "))
+third = int(input("Enter the last number here: "))
+# 2. find the lowest number
+
+# 3. display 
