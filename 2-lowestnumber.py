0294e7b497c4f9e9535d257973fc38de6ae6e851
unknown <giancarloestrella09@gmail.com>
2021-11-14 21:37:02 +0800

    Determines the lowest number among the set of given numbers using if-else statements.

--- 2-lowestnumber.py
+++ 2-lowestnumber.py
@@ -7,8 +7,14 @@
 def comparison(first, second, third):
     if first > second and second < third:
         print(f"The lowest number is the second number, {second}.")
+    elif third > first and second < first:
+        print(f"The lowest number is the second number, {second}.")
     elif first > third and second > third:
         print (f"The lowest number is the third number, {third}.")
+    elif second > third and second < first:
+        print (f"The lowest number is the third number, {third}.")
+    else:
+        print (f"The lowest number is the first number, {first}.")
    
 
 first = int(input("Enter the first number here: "))
